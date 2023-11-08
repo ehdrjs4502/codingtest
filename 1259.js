caee7a5ac9f686be0d61d2c5dd751857f2845c60
ehdrjs4502 <ehdrjs4502@naver.com>
2023-11-08 20:41:37 +0900

    코테

--- 1259.js
+++ 1259.js
@@ -1,23 +1,8 @@
-const fs = require('fs');
+const fs = require("fs");
 const input = fs.readFileSync("./input/1259.txt").toString().trim().split("\n");
 input.pop();
 
-let answer = "";
-
-for(let i = 0; i < input.length; i++) {
-    let count = 0;
-    const dup = Math.floor((input[i].length) / 2);
-    for(let j = 0; j < dup;  j++) {
-        if(input[i][j] == input[i][input[i].length - 1 - j]) {
-            count++;
-        }
-    }
-
-    if(count == dup) {
-        answer += "yes\n";
-    } else {
-        answer += "no\n";
-    }
+for (let i = 0; i < input.length; i++) {
+    const reverseStr = input[i].split("").reverse().join("");
+    console.log(input[i] === reverseStr ? "yes" : "no");
 }
-
-console.log(answer);
