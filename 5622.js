b70b96a8a6e54e5dd1a0946d356386e66d0e71fe
ehdrjs4502 <ehdrjs4502@naver.com>
2023-11-11 01:38:57 +0900

    코테

---
+++ 5622.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+const input = fs.readFileSync("./input/5622.txt").toString().trim();
+
+const dialNumber = {
+    A: 2,
+    B: 2,
+    C: 2,
+    D: 3,
+    E: 3,
+    F: 3,
+    G: 4,
+    H: 4,
+    I: 4,
+    J: 5,
+    K: 5,
+    L: 5,
+    M: 6,
+    N: 6,
+    O: 6,
+    P: 7,
+    Q: 7,
+    R: 7,
+    S: 7,
+    T: 8,
+    U: 8,
+    V: 8,
+    W: 9,
+    X: 9,
+    Y: 9,
+    Z: 9,
+};
+
+let result = 0;
+
+input.split("").map((item) => {
+    result += dialNumber[item];
+});
+
+console.log(result + input.length);
