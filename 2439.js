2ef05864027394123a0eef7eaaaafb3b4d1db4e3
ehdrjs4502 <ehdrjs4502@naver.com>
2023-11-22 23:33:03 +0900

    코테

--- 2439.js
+++ 2439.js
@@ -2,5 +2,5 @@ const fs = require("fs");
 const input = fs.readFileSync("./input/2439.txt").toString().trim();
 
 for (let i = 1; i <= input; i++) {
-  console.log(" ".repeat(input - i), "*".repeat(i));
+  console.log(" ".repeat(input - i) + "*".repeat(i));
 }
