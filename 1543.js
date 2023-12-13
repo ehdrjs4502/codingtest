0a261b8f0810f20ea458197ec85610c9708f7bdb
ehdrjs4502 <ehdrjs4502@naver.com>
2023-12-13 23:31:15 +0900

    코테(브루트포스 알고리즘)

--- 1543.js
+++ 1543.js
@@ -1,4 +1,22 @@
 const fs = require("fs");
-const input = fs.readFileSync("./input/1543.txt").toString().trim().split("\n");
+const input = fs
+  .readFileSync("./input/1543.txt")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((v) => v.trim());
 
-console.log(input);
+let str = input[0];
+let word = input[1];
+let result = 0;
+let idx = 0;
+
+while (idx >= 0) {
+  idx = str.indexOf(word);
+  if (idx >= 0) {
+    str = str.slice(idx + word.length, str.length);
+    result++;
+  }
+}
+
+console.log(result);
