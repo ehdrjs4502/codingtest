479b1890e8207abea0e110ae03f388b462456abd
ehdrjs4502 <ehdrjs4502@naver.com>
2023-12-16 15:56:48 +0900

    코테(그리디 알고리즘)

--- 14916.js
+++ 14916.js
@@ -1,14 +1,21 @@
 const fs = require("fs");
 const input = fs.readFileSync("./input/14916.txt").toString().trim();
-let cnt = 0;
-let num = Number(input);
 
-while (num > 0) {
-    if (num % 5 === 0) {
-        return console.log(num / 5 + cnt);
-    }
-    cnt++;
-    num -= 2;
+let num = Number(input);
+let result = 0;
+
+if (num == 1 || num == 3) {
+  return console.log(-1);
 }
 
-console.log(-1);
+while (num > 0) {
+  if (num % 2 === 1 || num % 5 === 0) {
+    result++;
+    num -= 5;
+  } else if (num % 2 === 0) {
+    result++;
+    num -= 2;
+  }
+}
+
+console.log(result);
