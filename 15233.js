fc8a9eeaf1b266490afaea5d49dfae928990ab54
ehdrjs4502 <ehdrjs4502@naver.com>
2023-12-21 00:32:49 +0900

    코테(해시를 사용한 집합과 맵)

--- 15233.js
+++ 15233.js
@@ -1,55 +1,41 @@
 const fs = require("fs");
-const readline = require("readline");
+const input = fs
+  .readFileSync("./input/15233.txt")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((v) => v.trim());
 
-const rl = readline.createInterface({
-  input: fs.createReadStream("./input/15233.txt"),
-  output: process.stdout,
-  terminal: false,
+input.shift();
+
+const players = new Map();
+let A = 0;
+let B = 0;
+
+input[0].split(" ").map((player) => {
+  players.set(player, "A");
 });
 
-const input = [];
-
-// 파일에서 한 줄씩 읽어옴
-rl.on("line", (line) => {
-  input.push(line.trim());
+input[1].split(" ").map((player) => {
+  players.set(player, "B");
 });
 
-// 모든 줄을 읽었을 때 실행
-rl.on("close", () => {
-  // 첫 번째 줄은 사용하지 않으므로 shift를 사용하여 제거
-  input.shift();
+if (input[2] === undefined) {
+  return console.log("TIE");
+}
 
-  const players = new Map();
-  let A = 0;
-  let B = 0;
-
-  // A 팀 선수들을 맵에 추가
-  input[0].split(" ").map((player) => {
-    players.set(player, "A");
-  });
-
-  // B 팀 선수들을 맵에 추가
-  input[1].split(" ").map((player) => {
-    players.set(player, "B");
-  });
-
-  // 골을 넣은 선수들을 확인하면서 A팀과 B팀의 점수를 계산
-  input[2].split(" ").map((goal) => {
-    players.get(goal) == "A" ? A++ : B++;
-  });
-
-  // A팀이 이긴 경우
-  if (A > B) {
-    return console.log("A");
-  }
-
-  // B팀이 이긴 경우
-  if (A < B) {
-    return console.log("B");
-  }
-
-  // 비긴 경우
-  if (A === B) {
-    return console.log("TIE");
-  }
+input[2].split(" ").map((player) => {
+  players.get(player) == "A" ? A++ : B++;
 });
+
+if (A > B) {
+  return console.log("A");
+}
+
+if (A < B) {
+  return console.log("B");
+}
+
+if (A === B) {
+  return console.log("TIE");
+}
