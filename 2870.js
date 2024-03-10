c263021992ba58d45706871c9425772aef3b3385
ehdrjs4502 <ehdrjs4502@naver.com>
2024-03-10 19:02:49 +0900

    코테(정렬)

--- 2870.js
+++ 2870.js
@@ -10,20 +10,11 @@ const n = input.shift();
 const result = [];
 
 for (let i = 0; i < n; i++) {
-  let num = "";
-  for (let j of input[i]) {
-    if (!isNaN(j)) {
-      num += j;
-    } else if (isNaN(j)) {
-      if (num.length > 0) {
-        result.push(BigInt(num));
-      }
-      num = "";
-    }
-  }
-  if (num.length > 0) {
-    result.push(BigInt(num));
+  const numbers = input[i].match(/\d+/g);
+  if (numbers) {
+    numbers.forEach((num) => result.push(num));
   }
 }
+result.sort((a, b) => a - b);
 
-console.log(result.sort((a, b) => (a < b ? -1 : a > b ? 1 : 0)).join("\n"));
+console.log(result.map(BigInt).join("\n"));
