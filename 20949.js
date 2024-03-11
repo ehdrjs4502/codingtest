1bd4df6bb4c7a3adac85d138e1c092e673d7629e
ehdrjs4502 <ehdrjs4502@naver.com>
2024-03-11 20:13:41 +0900

    코테(정렬)

---
+++ 20949.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync("./input/20949.txt")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((v) => v.trim());
+
+const n = Number(input.shift());
+
+const arr = [];
+
+for (let i = 0; i < n; i++) {
+  const [w, h] = input[i].split(" ").map(Number);
+
+  const ppi = Math.sqrt(w ** 2 + h ** 2) / 77;
+
+  arr.push([ppi, i + 1]);
+}
+
+arr.sort((a, b) => {
+  if (a[0] !== b[0]) return b[0] - a[0];
+
+  return a[1] - b[1];
+});
+
+console.log(arr.map((value) => value[1]).join("\n"));
