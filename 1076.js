e0b968ba80f78e89df770c1188436c628c3788bc
ehdrjs4502 <ehdrjs4502@naver.com>
2024-03-13 02:10:52 +0900

    코테(구현)

---
+++ 1076.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync("./input/1076.txt")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((v) => v.trim());
+
+const r = {
+  black: [0, 1],
+  brown: [1, 10],
+  red: [2, 100],
+  orange: [3, 1000],
+  yellow: [4, 10000],
+  green: [5, 100000],
+  blue: [6, 1000000],
+  violet: [7, 10000000],
+  grey: [8, 100000000],
+  white: [9, 1000000000],
+};
+
+let first = r[input[0]][0];
+let second = r[input[1]][0];
+let third = r[input[2]][1];
+
+console.log((first * 10 + second) * third);
