5c23055183a6ec21ac54cb9131ade6f758376750
ehdrjs4502 <ehdrjs4502@naver.com>
2024-04-03 16:47:56 +0900

    코테(재귀)

--- 10870.js
+++ 10870.js
@@ -1,13 +1,10 @@
-const fs = require('fs');
-const input = fs.readFileSync("./input/2747.txt").toString().trim();
+const fs = require("fs");
+const input = fs.readFileSync("./input/10870.txt").toString().trim();
 
 function fibo(num) {
-
-    if(num === 1 || num === 2) {
-        return 1;
-    }
-
-    return fibo(num-1) + fibo(num-2);
+  if (num === 0) return 0;
+  if (num === 1 || num === 2) return 1;
+  return fibo(num - 1) + fibo(num - 2);
 }
 
-console.log(fibo(20));
+console.log(fibo(+input));
