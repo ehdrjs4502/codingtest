d19f5a1359bcb880af02a614f2ddc99f1a24f0b5
ehdrjs4502 <ehdrjs4502@naver.com>
2024-04-30 00:42:55 +0900

    코테(이분 탐색, 해시를 사용한 집합과 맵)

--- 10815.js
+++ 10815.js
@@ -1,16 +1,46 @@
-const fs = require('fs');
+const fs = require("fs");
 const input = fs.readFileSync("./input/10815.txt").toString().trim().split("\n");
 
-const [N, M] = [Number(input[0]), Number(input[2])];
+function binarySearch(arr, target) {
+  let start = 0;
+  let end = arr.length - 1;
+  let mid = Math.floor((start + end) / 2);
 
-//상근이가 가지고 있는 카드 set에 저장
-const [myCards,cards] = [new Set(input[1].split(' ').map(Number)),input[3].split(' ').map(Number)]
+  while (end - start >= 0) {
+    if (target == arr[mid]) {
+      return 1;
+    } else if (arr[mid] < target) {
+      start = mid + 1;
+    } else {
+      end = mid - 1;
+    }
 
-let result = ''
-
-// 상근이가 가지고 있는 카드 중 찾는 카드가 있는 지 확인해서 있으면 1 없으면 0
-for(let i = 0; i < M; i++) {
-    myCards.has(cards[i]) === true ? result += 1 + ' ' : result += 0 + ' '
+    mid = Math.floor((start + end) / 2);
+  }
+  return 0;
 }
 
-console.log(result)
+const useCards = input[1]
+  .split(" ")
+  .map(Number)
+  .sort((a, b) => a - b);
+const cards = input[3].split(" ").map(Number);
+const result = [];
+
+for (let card of cards) {
+  result.push(binarySearch(useCards, card));
+}
+
+console.log(result.join(" "));
+
+// const [N, M] = [Number(input[0]), Number(input[2])];
+
+// //상근이가 가지고 있는 카드 set에 저장
+// const [myCards,cards] = [new Set(input[1].split(' ').map(Number)),input[3].split(' ').map(Number)]
+
+// let result = ''
+
+// // 상근이가 가지고 있는 카드 중 찾는 카드가 있는 지 확인해서 있으면 1 없으면 0
+// for(let i = 0; i < M; i++) {
+//     myCards.has(cards[i]) === true ? result += 1 + ' ' : result += 0 + ' '
+// }
--- 10816.js
+++ 10816.js
@@ -1,19 +1,55 @@
 const fs = require("fs");
 const input = fs.readFileSync("./input/10816.txt").toString().trim().split("\n");
 
-const cards = input[1].split(" ");
-const matchCards = input[3].split(" ");
+function binarySearch(arr, target) {
+  let start = 0;
+  let end = arr.length - 1;
+  let mid = Math.floor((start + end) / 2);
+  while (end - start >= 0) {
+    if (target == arr[mid]) {
+      return useCardCount.get(target);
+    } else if (arr[mid] < target) {
+      start = mid + 1;
+    } else {
+      end = mid - 1;
+    }
 
-let map = new Map();
-
-let result = [];
-
-for (let i = 0; i < cards.length; i++) {
-    map.has(cards[i]) ? map.set(cards[i], map.get(cards[i]) + 1) : map.set(cards[i], 1);
+    mid = Math.floor((start + end) / 2);
+  }
+  return 0;
 }
 
-for (let i = 0; i < matchCards.length; i++) {
-    map.has(matchCards[i]) ? result.push(map.get(matchCards[i])) : result.push(0);
+const useCards = input[1]
+  .split(" ")
+  .map(Number)
+  .sort((a, b) => a - b);
+const cards = input[3].split(" ").map(Number);
+const useCardCount = new Map();
+const result = [];
+
+for (let i = 0; i < useCards.length; i++) {
+  useCardCount.set(useCards[i], (useCardCount.get(useCards[i]) || 0) + 1);
+}
+
+for (let i = 0; i < cards.length; i++) {
+  result.push(binarySearch(useCards, cards[i]));
 }
 
 console.log(result.join(" "));
+
+// const cards = input[1].split(" ");
+// const matchCards = input[3].split(" ");
+
+// let map = new Map();
+
+// let result = [];
+
+// for (let i = 0; i < cards.length; i++) {
+//     map.has(cards[i]) ? map.set(cards[i], map.get(cards[i]) + 1) : map.set(cards[i], 1);
+// }
+
+// for (let i = 0; i < matchCards.length; i++) {
+//     map.has(matchCards[i]) ? result.push(map.get(matchCards[i])) : result.push(0);
+// }
+
+// console.log(result.join(" "));
