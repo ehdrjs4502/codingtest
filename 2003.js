28fd342c3f0a69e653b39d658762038847375b29
ehdrjs4502 <ehdrjs4502@naver.com>
2024-05-26 22:25:45 +0900

    코테(투포인터)

---
+++ 2003.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const input = fs.readFileSync("./input/2003.txt").toString().trim().split("\n");
+
+const [n, m] = input.shift().split(" ").map(Number);
+const arr = input.shift().split(" ").map(Number);
+
+let start = 0;
+let end = 0;
+let sum = 0;
+let result = 0;
+
+while (end <= n) {
+  if (sum == m) {
+    result++;
+  }
+
+  if (sum >= m) {
+    sum -= arr[start++];
+  } else {
+    sum += arr[end++];
+  }
+
+  console.log(arr[start], arr[end]);
+}
+
+console.log(arr);
