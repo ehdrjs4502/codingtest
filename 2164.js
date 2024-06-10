9101691ea2f32d7d7246f43eaa9966e761fa3d43
ehdrjs4502 <ehdrjs4502@naver.com>
2024-06-10 12:07:10 +0900

    코테(큐[연결리스트])

--- 2164.js
+++ 2164.js
@@ -1,15 +1,66 @@
 const fs = require("fs");
 const input = fs.readFileSync("./input/2164.txt").toString().trim();
 
-const arr = Array(Number(input))
-  .fill()
-  .map((v, i) => i + 1);
-
-while (true) {
-  if (arr.length === 1) {
-    return console.log(arr[0]);
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
   }
-  arr.shift();
-  const shuffle = arr.shift();
-  arr.push(shuffle);
 }
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  // 요소를 리스트의 끝에 추가
+  add(data) {
+    const newNode = new Node(data);
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.size++;
+  }
+
+  // 리스트의 헤드에서 요소를 제거
+  removeHead() {
+    if (this.head === null) {
+      return console.log("List is empty");
+    } else {
+      const removedData = this.head.data;
+      this.head = this.head.next;
+      this.size--;
+      return removedData;
+    }
+  }
+
+  // 리스트 출력
+  printList() {
+    let current = this.head;
+    let result = "";
+    while (current) {
+      result += current.data;
+      current = current.next;
+    }
+    console.log(result);
+  }
+}
+
+const cards = new LinkedList();
+
+for (let i = 1; i <= input; i++) {
+  cards.add(i);
+}
+
+while (cards.size > 1) {
+  cards.removeHead();
+  cards.add(cards.head.data);
+  cards.removeHead();
+}
+
+cards.printList();
