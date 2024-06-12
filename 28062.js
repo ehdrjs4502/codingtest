6471931432233ac33ddc50a2be4c73c275916a32
ehdrjs4502 <ehdrjs4502@naver.com>
2024-06-12 22:55:54 +0900

    코테(그리디 알고리즘)

--- 28062.js
+++ 28062.js
@@ -6,32 +6,32 @@ const input = fs
   .split("\n")
   .map((v) => v.trim());
 
-input.shift();
+const n = +input.shift();
+const arr = input
+  .shift()
+  .split(" ")
+  .map(Number)
+  .sort((a, b) => a - b);
 
-let result = 0;
-const oddArr = [];
-
-function sum(arr) {
-  return arr.reduce((acc, curr) => {
-    return acc + curr;
-  }, 0);
-}
-
-input[0].split(" ").map((value) => {
-  if (value % 2 == 1) {
-    oddArr.push(+value);
+if (n === 1) {
+  if (arr[0] % 2 === 0) {
+    return console.log(arr[0]);
   } else {
-    result += +value;
+    return console.log(0);
   }
-});
-
-if (oddArr.length % 2 == 1) {
-  const sortOddArr = oddArr.sort();
-  sortOddArr.shift();
-
-  result += sum(sortOddArr);
-} else {
-  result += sum(oddArr);
 }
 
-console.log(result % 2 == 0 ? result : 0);
+let sum = arr.reduce((a, b) => a + b);
+
+if (sum % 2 !== 0) {
+  for (let i = 0; i < n; i++) {
+    if (arr[i] % 2 !== 0) {
+      sum -= arr[i];
+      break;
+    }
+  }
+
+  return console.log(sum);
+}
+
+console.log(sum);
